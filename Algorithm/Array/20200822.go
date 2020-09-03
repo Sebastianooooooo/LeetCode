20af808c8757a62e52cbaf13cfcd8b10840fc802
guanlh <745271634@qq.com>
2020-09-03 17:06:46 +0800

    8.22

--- Algorithm/Array/20200822.go
+++ Algorithm/Array/20200822.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
-	
+	r1 := threeSum([]int{-1, 0, 1, 2, -1, -4})
+	fmt.Println(r1)
 }
 
 
@@ -20,24 +26,37 @@ func main() {
  *
  *
 */
-func threeSum(height []int) int {
-	max, start, end := 0, 0, len(height)-1
-	for start < end {
-		width := end - start
-		high := 0
-		if height[start] < height[end] {
-			high = height[start]
-			start++
-		} else {
-			high = height[end]
-			end--
+func threeSum(nums []int) [][]int {
+
+	var res [][]int
+	counter := map[int]int{}
+	for _, value := range nums {
+		counter[value]++
+	}
+	var uniqNums []int
+	for key := range counter {
+		uniqNums = append(uniqNums, key)
+	}
+	sort.Ints(uniqNums)
+	for i := 0; i < len(uniqNums); i++ {
+		if (uniqNums[i]*3 == 0) && counter[uniqNums[i]] >= 3 { //有3个值都是0的
+			res = append(res, []int{uniqNums[i], uniqNums[i], uniqNums[i]})
 		}
-		temp := width * high
-		if temp > max {
-			max = temp
+		for j := i + 1; j < len(uniqNums); j++ {
+			if (uniqNums[i]*2+uniqNums[j] == 0) && counter[uniqNums[i]] > 1 {
+				res = append(res, []int{uniqNums[i], uniqNums[i], uniqNums[j]})
+			}
+			if (uniqNums[j]*2+uniqNums[i] == 0) && counter[uniqNums[j]] > 1 {
+				res = append(res, []int{uniqNums[i], uniqNums[j], uniqNums[j]})
+			}
+			c := 0 - uniqNums[i] - uniqNums[j]
+			if c > uniqNums[j] && counter[c] > 0 {
+				res = append(res, []int{uniqNums[i], uniqNums[j], c})
+			}
 		}
 	}
-	return max
+	return res
+
 }
 
 
